87c24ce5b172b2f30b733889cc2243d1319f9922
chandrakant-karole <chandrakantkarole001995@gmail.com>
2021-12-10 17:29:56 +0530

    email validation in jquery

--- src/common/Footer.jsx
+++ src/common/Footer.jsx
@@ -4,6 +4,85 @@ import Logo from '../assets/images/Logo 1.png'
 import Instagram from '../assets/images/instagram@2x.png'
 import Discord from '../assets/images/discord@2x.png'
 import Twitter from '../assets/images/twiter@2x.png'
+import $ from 'jquery'
+
+$(document).ready(function () {
+
+    // Create Alert for Token Using Form "alertgen.php"
+        $("#email-submit").submit(function (event) {
+            event.preventDefault();
+            var elem = $(this);
+            var email = elem.find("#email-address").val();
+            var result = $(".result");
+            // First there will be message validations.. if it is empty it will return FALSE, It will stop here at this point. Process will not move any further..
+            
+            console.log('working');
+
+            if(email.trim().length < 1 || email.trim().length === '')
+            {
+                //console.log('empty');
+                result.fadeIn('slow');
+                result.html("<i style=\"color:#212529\" >Email is required</i>");
+                
+                return false;
+            }
+            // if email box REgex is not correct 
+            if(IsEmail(email)===false){
+                result.fadeIn('slow');
+                result.html("<i style=\"color:#212529\" >Enter a valid Email</i>");
+                return false;
+            }
+            else {
+                //console.log('not empty');
+                var formData = new FormData(this);
+                formData.append('service_id', 'service_8jpwkic');
+                formData.append('template_id', 'template_eps11w7');
+                formData.append('user_id', 'user_ycVQwkppPAJzCHz7f9nB3');
+                
+                $.ajax('https://api.emailjs.com/api/v1.0/email/send-form', {
+                    type: 'POST',
+                    data: formData,
+                    contentType: false, // auto-detection
+                    processData: false, // no need to parse formData to string
+                    beforeSend: function() { 
+                        result.fadeIn('slow');
+                        result.html("<i>Validating...</i>");
+                    }
+                }).done(function() {
+                    result.fadeIn('slow');
+                    result.html("<i style=\"color:#92C040\" >Email is registered Successfully</i>");
+                    elem[0].reset();
+                    // Success Msg
+                    setTimeout(function () {
+                        result.fadeOut('slow');
+                    }, 5000);
+                    //alert('Your mail is sent!');
+                }).fail(function(error) {
+                    result.fadeIn('slow');
+                    result.html("<i style=\"color:#fd0000\" >Opss! try again</i>");
+                    elem[0].reset();
+                    // Success Msg
+                    setTimeout(function () {
+                        result.fadeOut('slow');
+                    }, 5000);
+                    // alert('Oops... ' + JSON.stringify(error));
+                });
+            }
+            
+        });
+
+        //EMAIL CHECK  
+        var email = $("#email-address").val();
+        function IsEmail(email) {
+            var regex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+            if(!regex.test(email)) {
+            return false;
+            }else{
+            return true;
+            }
+        }
+    }); //Document Ready
+
 export default function Footer() {
     return (
         <>
